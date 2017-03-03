1456c889d9a7a36388eadb361734e4b5ba38789f
cherylyli <cherylliat@gmail.com>
2017-03-03 11:56:28 -0500

    prettify map

--- public/js/board.js
+++ public/js/board.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
         for (var j = 0; j < 4; j++) {
             createAndPlaceAtLocation(hex_width * j, ((3 - j) * (hex_height / 2.0)), getHexId());
             if (j == 3) {
-                createVertice(hex_width * j + hex_width + "px", ((3 - j) * (hex_height / 2.0)) + "px", getVerticeId());
+                createVertice(hex_width * j + 188 + "px", ((3 - j) * (hex_height / 2.0)) + "px", getVerticeId());
             }
         }
 
@@ -56,7 +56,7 @@ $(document).ready(function() {
         for (var j = 0; j < 5; j++) {
             createAndPlaceAtLocation(hex_width * j, ((5 - j) * (hex_height / 2.0)), getHexId());
             if (j == 4) {
-                createVertice(hex_width * j + hex_width + "px", ((5 - j) * (hex_height / 2.0)) + "px", getVerticeId());
+                createVertice(hex_width * j + 223 + "px", ((5 - j) * (hex_height / 2.0)) + 24 +  "px", getVerticeId());
             }
         }
 
@@ -64,7 +64,7 @@ $(document).ready(function() {
         for (var j = 0; j < 6; j++) {
             createAndPlaceAtLocation(hex_width * j, ((7 - j) * (hex_height / 2.0)), getHexId());
             if (j == 5) {
-                createVertice(hex_width * j + hex_width + "px", ((7 - j) * (hex_height / 2.0)) + "px", getVerticeId());
+                createVertice(hex_width * j + 215 + hex_width + "px", ((7 - j) * (hex_height / 2.0)) + 50+ "px", getVerticeId());
             }
         }
 
@@ -74,33 +74,31 @@ $(document).ready(function() {
                 if (j >= k - delta1) {
                     createAndPlaceAtLocation(hex_width * j, ((7 + (2 * (k + 1)) - j) * (hex_height / 2.0)), getHexId());
                     if (j == 6) {
-                        createVertice(hex_width * j + hex_width + "px", ((9 + (2 * k) - j) * (hex_height / 2.0)) + "px", getVerticeId());
+                        createVertice(hex_width * j + hex_width  + 253 + "px", ((9 + (2 * k) - j) * hex_height) + "px", getVerticeId());
                     }
                     if (k > 0 && j == 6) {
-                        createVertice(hex_width * j + hex_width + hex_width_correction + "px", (((9 + (2 * k) - j) - 1) * (hex_height / 2.0)) + "px", getVerticeId());
+                        createVertice(hex_width * j  + hex_width + 273 +  hex_width_correction + "px", (((9 + (2 * k) - j) - 1) * hex_height) + "px", getVerticeId());
                     }
                 } else if (j == k - delta1 - 1) {
-                    createVertice(hex_width * j + hex_width_correction + "px", ((9 + (2 * k) - j) * (hex_height / 2.0)) + "px", getVerticeId());
+                    createVertice(hex_width * j*1.77 + 28 + "px", ((9 + (2 * k) - j) * hex_height) +3+ "px", getVerticeId());
                 }
             }
         }
 
         //Places vertices below on the last diagonal line of hexes
         for (var i = 0; i < 4; i++) {
-            createVertice(hex_width * (3 + i) + hex_width_correction + "px", ((14 + (delta1 * 2) - i) * (hex_height / 2.0)) + "px", getVerticeId());
-            createVertice(hex_width * (4 + i) - 10 + "px", ((14 + (delta1 * 2) - i) * (hex_height / 2.0)) + "px", getVerticeId());
+            createVertice(hex_width * i*1.77 + 272 + "px", ((14 + (delta1 * 2) - i) * hex_height ) + 3+ "px", getVerticeId());
+            createVertice(hex_width * i * 1.77 + 325 + "px", ((14 + (delta1 * 2) - i) * hex_height ) +3 + "px", getVerticeId());
             if (i == 3) {
-                createVertice(hex_width * (4 + i) + hex_width_correction + "px", ((13 + (delta1 * 2) - i) * (hex_height / 2.0)) + "px", getVerticeId());
+                createVertice(hex_width * i + +458 + "px", ((13 + (delta1 * 2) - i) * hex_height) + 1+ "px", getVerticeId());
             }
         }
     }
 
     function createAndPlaceAtLocation(x, y, id) {
-        // createVertice(x - 10 + "px", (y + hex_height / 2) + "px", getVerticeId());
-        // createVertice(x+ hex_width_correction + "px", y + "px", getVerticeId());
-
-        // createVertice(x*2.25 - 20 + "px", (y*2.25 + hex_height / 2) + "px", getVerticeId());
-        createVertice(x*1.6+ 100 + "px", y*2.25 + 20 + "px", getVerticeId());
+        createVertice(x*1.8  + "px", (y*2.0 + hex_height) + "px", getVerticeId());
+        createVertice(x*1.8 + 25 + "px", y*2.0 + "px", getVerticeId());
+        
 
         x = x *1.8+ "px";
         y = y *2.0+ "px";
