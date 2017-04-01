26c0a3c3c88c568bee490e02b3a8e623f7d9657e
EmolLi <duan.li@mail.mcgill.ca>
2017-04-01 14:26:37 -0400

    close cmd prompt automatically

--- public/js/room.js
+++ public/js/room.js
@@ -659,6 +659,7 @@ $(window).on('imready', function(im){
             else {
                 let cmdName = $button.attr('cmd');
                 Commands[cmdName].apply(this, data);
+                hideCmdPrompt()
                 console.log($button.attr('cmd'));
             }
 
